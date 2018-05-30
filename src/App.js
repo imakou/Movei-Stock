4f7ba704cadff198f0980f599e54469aee6adc7b
Allen Kou <imakou@gmail.com>
2018-05-30 18:14:07 -0400

    init: import Bootstrap/ANTD

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './sass/test.css'
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Nav from "./components/Nav";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Nav />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3">
+                <p>4</p>
+              </div>
+              <div className="col-md-3">
+                <p>4</p>
+              </div>
+              <div className="col-md-3">
+                <p>43</p>
+              </div>
+            </div>
+          </div>
         </p>
       </div>
     );
