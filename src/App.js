de4b86309398d3bbbda2849de9e17c540d9fd544
Allen Kou <imakou@gmail.com>
2018-05-31 18:00:39 -0400

    add: JumBotron

--- src/App.js
+++ src/App.js
@@ -7,25 +7,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header>
           <Nav />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <div className="container">
-            <div className="row">
-              <div className="col-md-3">
-                <p>4</p>
-              </div>
-              <div className="col-md-3">
-                <p>4</p>
-              </div>
-              <div className="col-md-3">
-                <p>43</p>
+        <section className="AppJumbotron d-flex align-items-end justify-content-center">
+          <div className="AppDescription">
+            <div className="container ">
+              <div className="row ">
+                <div className="col-7">
+                  <h1>gggggg</h1>
+                  <p>
+                    This method is like _.difference except that it accepts iteratee which
+                    is invoked for each element of array and values to generate the
+                    criterion by which they're compared. The order and references of
+                    result values are determined by the first array. The iteratee is
+                    invoked with one argument: (value).
+                  </p>
+                </div>
+                <div className="col-5">
+                  <h1>fwefnwelnfk</h1>
+                </div>
               </div>
             </div>
           </div>
-        </p>
+        </section>
       </div>
     );
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,43 +7,45 @@ class Nav extends Component {
     return (
       <React.Fragment>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <img src={logo} alt="" />
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
+          <div className="container">
+            <img className="NavLogo" src={logo} alt="" />
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Home <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-            </ul>
-            <form className="form-inline my-2 my-lg-0">
-              <input
-                className="form-control mr-sm-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-                Search
-              </button>
-            </form>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">
+                    Home <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+              </ul>
+              <form className="form-inline my-2 my-lg-0">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
           </div>
         </nav>
       </React.Fragment>
