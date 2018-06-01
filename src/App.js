633d5e7cba33d999e8dce7c83f2b865fa4393609
Allen Kou <allen@PC-CHEN-7.local>
2018-06-01 08:45:53 -0400

    add: grid

--- src/App.js
+++ src/App.js
@@ -17,11 +17,10 @@ class App extends Component {
                 <div className="col-7">
                   <h1>gggggg</h1>
                   <p>
-                    This method is like _.difference except that it accepts iteratee which
-                    is invoked for each element of array and values to generate the
-                    criterion by which they're compared. The order and references of
-                    result values are determined by the first array. The iteratee is
-                    invoked with one argument: (value).
+                    This method is like _.difference except that it accepts iteratee which is invoked for
+                    each element of array and values to generate the criterion by which they're compared.
+                    The order and references of result values are determined by the first array. The
+                    iteratee is invoked with one argument: (value).
                   </p>
                 </div>
                 <div className="col-5 pb-5 d-flex align-items-end justify-content-center">
@@ -39,6 +38,32 @@ class App extends Component {
             </div>
           </div>
         </section>
+        <div className="container">
+          <div className="row ">
+            <div className="col-3">
+              <div className="border shadow-sm">
+                <img
+                  className="img-fluid"
+                  src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg"
+                  alt=""
+                />
+                <div className="p-5 d-flex justify-content-around">
+                  <div>1234</div>
+                  <div class="badge badge-pill badge-info">Info</div>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="border">1234</div>
+            </div>
+            <div className="col-3">
+              <div className="border">1234</div>
+            </div>
+            <div className="col-3">
+              <div className="border">1234</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
