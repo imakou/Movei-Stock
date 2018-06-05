58045e17e2a5a182cbff9d34a2c9e6e7f15e8d5d
Allen Kou <allen@PC-CHEN-7.local>
2018-06-04 23:36:05 -0400

    add: detail page jumborton

--- src/containers/MovieDetail.js
+++ src/containers/MovieDetail.js
@@ -15,13 +15,35 @@ class MovieDetail extends Component {
             backgroundImage:
               'url("https://image.tmdb.org/t/p/w1400_and_h450_face/gBmrsugfWpiXRh13Vo3j0WW55qD.jpg")'
           }}
-          className="AppJumbotron d-flex align-items-stretch"
+          className="AppJumbotron d-flex align-items-stretch position-relative"
         >
-          <div className="w-100" style={{ backgroundColor: "rgba(46, 85, 81, 0.8)" }} />
+          <div
+            className="w-100 MovieDetailJumbotronMask"
+            style={{ backgroundColor: "rgba(46, 85, 81, 0.8)" }}
+          />
+          <div className="container MovieDetailJumbotron">
+            <div className="row">
+              <div className="col-md-4 col-sm-12 p-4">
+                <img
+                  className="img-fluid"
+                  src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/ufoGrTRbItHvqk42yNHcyoE0afM.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="col-md-8 col-sm-12 p-4">
+                <h3>Supergirl</h3>
+                <h4>Overview</h4>
+                <p>
+                  Twenty-four-year-old Kara Zor-El, who was taken in by the Danvers family
+                  when she was 13 after being sent away from Krypton, must learn to
+                  embrace her powers after previously hiding them. The Danvers teach her
+                  to be careful with her powers, until she has to reveal them during an
+                  unexpected disaster, setting her on her journey of heroism.
+                </p>
+              </div>
+            </div>
+          </div>
         </section>
-        <div className="container MovieDetailJumbotron">
-          <h1>333</h1>
-        </div>
       </React.Fragment>
     );
   }
