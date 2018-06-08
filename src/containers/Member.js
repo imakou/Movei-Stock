b97a4bb0defc4c2d829e5374f02884efa006bcea
Allen Kou <allen@PC-CHEN-7.local>
2018-06-07 23:45:35 -0400

    modify: memberJumbortonInfo

--- src/containers/Member.js
+++ src/containers/Member.js
@@ -17,24 +17,31 @@ class Member extends Component {
             backgroundImage:
               'url("https://image.tmdb.org/t/p/w1400_and_h450_face/kqoBtMmiycbbhGLXGkKhL8SdaWB.jpg")'
           }}
-          className="MemberJumbotron d-flex align-items-stretch position-relative"
+          className="MemberJumbotron d-flex align-items-end"
         >
-          <div
+          {/* <div
             className="w-100 MovieDetailJumbotronMask"
             style={{ backgroundColor: "rgba(113, 8, 9, 0.8)" }}
-          />
-          <div className="container MovieDetailJumbotron">
-            <div className="row">
-              <div className="col-md-12">
-                <div>
-                  <span>
+          /> */}
+          <div className="MemberJumbotronInfo w-100">
+            <div className="container MovieDetailJumbotron">
+              <div className="row">
+                <div className="col-md-12">
+                  <div>
                     <h1>Imakou</h1>
-                  </span>
-                  <span>
-                    <h6>
-                      <small>2018/06/05</small>
-                    </h6>
-                  </span>
+                    <div className="row MemberJumbotronDesc d-flex justify-content-start">
+                      <div className="col-md-4 col-sm-12">
+                        <h6>
+                          <small>Member Type: Standard</small>
+                        </h6>
+                      </div>
+                      <div className="col-md-4 col-sm-12">
+                        <h6>
+                          <small>Standard Member</small>
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
