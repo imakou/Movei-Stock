97d580027efc060ae0a30028bb58493c01b5fd9d
Allen Kou <imakou@gmail.com>
2018-07-24 18:29:58 -0400

    fefactor: action/reducer

--- src/reducer/movie.js
+++ src/reducer/movie.js
@@ -9,24 +9,24 @@ const initialState = {
   currentMovie: null
 };
 
-export const movies = (state = initialState, { type, payload }) => {
-  switch (type) {
+export const movies = (state = initialState, action) => {
+  switch (action.type) {
     case MOVIE_ACTIONS.FETCH_POP_MOVIES_SUCCESSFUL:
       return {
         ...state,
-        popMovies: payload
+        popMovies: action.popMovies
       };
 
     case MOVIE_ACTIONS.FETCH_NOW_PLAYING_MOVIES_SUCCESSFUL:
       return {
         ...state,
-        nowPlayingMovies: payload
+        nowPlayingMovies: action.payload
       };
 
     case MOVIE_ACTIONS.FETCH_MOVIE_DETAIL_SUCCESSFUL:
       return {
         ...state,
-        currentMovie: payload
+        currentMovie: action.payload
       };
 
     case MOVIE_ACTIONS.EMPTY_MOVIE_DETAIL_SUCCESSFUL:
@@ -37,20 +37,20 @@ export const movies = (state = initialState, { type, payload }) => {
     case MOVIE_ACTIONS.SEARCH_MOVIES_SUCCESSFUL:
       return {
         ...state,
-        searchedMovies: payload.data,
-        searchedMoviesTotalPage: payload.page
+        searchedMovies: action.payload.data,
+        searchedMoviesTotalPage: action.payload.page
       };
 
     case MOVIE_ACTIONS.FETCH_MORE_MOVIES_SUCCESSFUL:
       return {
         ...state,
-        searchedMovies: payload
+        searchedMovies: action.payload
       };
 
     case MOVIE_ACTIONS.UPDATE_KEYWORD_SUCCESSFUL:
       return {
         ...state,
-        keyWord: payload
+        keyWord: action.payload
       };
 
     case MOVIE_ACTIONS.EMPTY_SEARCH_MOVIES_SUCCESSFUL:
